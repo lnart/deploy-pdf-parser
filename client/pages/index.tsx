f8419f333ef2d814562e75bce385347cc4f152b0
Lennart <115736797+lnart@users.noreply.github.com>
2023-11-23 10:14:50 +0100

    change folder structure

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import Cookies from 'js-cookie';
-import { validateToken, uploadFile, fetchUploadedFiles, deleteFile } from './indexRequests'; 
+import { validateToken, uploadFile, fetchUploadedFiles, deleteFile } from '../public/indexRequests'; 
 import styles from '../styles/home.module.css'
 
 
