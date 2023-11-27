418f59eade0769836c7e9129105173f29b1b7c21
Lennart <115736797+lnart@users.noreply.github.com>
2023-11-27 18:06:20 +0100

    fix upload path

--- server/src/multerConfig/multer.ts
+++ server/src/multerConfig/multer.ts
@@ -2,9 +2,12 @@ import multer from 'multer'
 import path from 'path'
 import fs from 'fs'
 
+const uploadsPath = path.join(__dirname, 'uploads');
+
+
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, './uploads');
+      cb(null, uploadsPath);
     },
     filename: function (req, file, cb) {
       if (!file.originalname) {
